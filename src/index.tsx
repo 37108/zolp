5cf226e52f878b2937611ba31e292e20a4abf747
37108 <37108mail@gmail.com>
2025-09-05 16:51:37 +0900

    init

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import { Editor } from "./components/editor";
+
+const container = document.getElementById("root");
+if (!container) throw new Error("Failed to find the root element");
+
+if (import.meta.env.PROD) {
+	createRoot(container).render(
+		<StrictMode>
+			<Editor />
+		</StrictMode>,
+	);
+} else {
+	createRoot(container).render(
+		<StrictMode>
+			<Editor />
+		</StrictMode>,
+	);
+}
